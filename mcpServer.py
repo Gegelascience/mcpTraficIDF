29c76baad6c751be2524ba286d198aae0c4219d3
VERCHERE Rémi <remi.verchere2@gmail.com>
2026-01-03 14:46:37 +0100

    bascule input enum

--- mcpServer.py
+++ mcpServer.py
@@ -2,7 +2,8 @@
 from mcp.server.fastmcp import FastMCP
 import os
 import argparse
-from services.idf_transport import getLineInfo
+from services.idf_transport import LineIDF, getLineInfo
+from typing import Any
 
 import logging
 logger = logging.getLogger(__name__)
@@ -14,40 +15,23 @@ from dotenv import load_dotenv
 
 
 # Create an MCP server
-mcp = FastMCP("IDFTrafficInfo")
+mcp = FastMCP("IDFTrafficInfo", "Get traffic information for the Paris region transport lines (Metro, RER, Transilien)")
 
 
-# Add an addition tool
-@mcp.tool()
-def sum(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
 
 # get transport line information
 @mcp.tool()
-def get_transport_info(line: str) -> list[list[dict]] | str:
-    """Get information about a transport line in the Paris region. It can be a metro line or a RER line"""
+def get_transport_info(line: LineIDF) -> list[list[dict[str,Any]]] | str:
+    """
+        Get information about a transport line in the Paris region. It can be a metro line or a RER line or transilien line.
+        Args:
+            line (LineIDF): The line to get information about. It should be one of the values from the LineIDF enum.
+    """
     apikey = os.environ.get("IDF_API_KEY", "")
     logger.info(f"API key: {apikey}")
     
     return getLineInfo(apikey, line)
 
-
-@mcp.prompt()
-def get_transport_info_prompt(stifData: str) -> str:
-    """Prompt for transport line information."""
-    if not stifData:
-        return "Aucune information disponible pour cette ligne de transport."
-    
-    logger.info(f"Received STIF data: {stifData}.")
-
-    return f"Voici les informations sur la ligne de transport : {stifData}. La réponse est formatée en chaine de caractères."
-
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
 
 
 if __name__ == "__main__":
--- services/idf_transport.py
+++ services/idf_transport.py
@@ -1,5 +1,6 @@
+from typing import Any
 import requests
-
+from enum import Enum
 import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
@@ -7,74 +8,92 @@ logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBU
 hostUrl = "https://prim.iledefrance-mobilites.fr/marketplace"
 
 
-def formatMsgIdf(listMsg:list[str]) -> list[dict]:
+class LineIDF(Enum):
+    LINE_1 = "1"
+    LINE_2 = "2"
+    LINE_3 = "3"
+    LINE_3BIS = "3 bis"
+    LINE_4 = "4"
+    LINE_5 = "5"
+    LINE_6 = "6"
+    LINE_7 = "7"
+    LINE_7BIS = "7 bis"
+    LINE_8 = "8"
+    LINE_9 = "9"
+    LINE_10 = "10"
+    LINE_11 = "11"
+    LINE_12 = "12"
+    LINE_13 = "13"
+    LINE_14 = "14"
+    TRANSILIEN_R = "R"
+    TRANSILIEN_H = "H"
+    RER_A = "A"
+    RER_B = "B"
+    RER_C = "C"
+    RER_D = "D"
+    RER_E = "E"
+    TRANSILIEN_P = "P"
+    TRANSILIEN_J = "J"
+    TRANSILIEN_N = "N"
+    TRANSILIEN_L = "L"
+    TRANSILIEN_K = "K"
+    TRANSILIEN_V = "V"
+    TRANSILIEN_U = "U"
+
+def formatMsgIdf(listMsg:list[dict[str,Any]]) -> list[dict[str,Any]]:
     if len(listMsg) == 0:
         return []
     else:
-        return [{"type":msg.get("InfoChannelRef", {}).get("value", {}),"message":msg.get("Content", {}).get("Message", [])[0].get("MessageText",{}).get("value","") } for msg in listMsg]
+        return [{"type":dict(msg.get("InfoChannelRef", {})).get("value", {}),"message":dict(msg.get("Content", {})).get("Message", [])[0].get("MessageText",{}).get("value","") } for msg in listMsg]
 
 
 def getLineStif(line: str) -> str:
 
-    if line == "1":
-        return "C01371"
-    elif line == "2":
-        return "C01372"
-    elif line == "3":
-        return "C01373"
-    elif line == "3bis" or line == "3 bis":
-        return "C01386"
-    elif line == "4":
-        return "C01374"
-    elif line == "5":
-        return "C01375"
-    elif line == "6":
-        return "C01376"
-    elif line == "7":
-        return "C01377"
-    elif line == "7bis" or line == "7 bis":
-        return "C01387"
-    elif line == "8":
-        return "C01378"
-    elif line == "9":
-        return "C01379"
-    elif line == "10":
-        return "C01380"
-    elif line == "11":
-        return "C01381"
-    elif line == "12":
-        return "C01382"
-    elif line == "13":
-        return "C01383"
-    elif line == "14":
-        return "C01384"
-    elif line == "R":
-        return "C01731"
-    elif line == "H":
-        return "C01737"
-    elif line == "A" or line == "RER A":
-        return "C01742"
-    elif line == "B" or line == "RER B":
-        return "C01743"
-    elif line == "C" or line == "RER C":
-        return "C01727"
-    elif line == "D" or line == "RER D":
-        return "C01728"
-    elif line == "E" or line == "RER E":
-        return "C01729"
-    else:
-        return ""
+    mappingLineToStif = {
+        "1": "C01371",
+        "2": "C01372",
+        "3": "C01373",
+        "3 bis": "C01386",
+        "4": "C01374",
+        "5": "C01375",
+        "6": "C01376",
+        "7": "C01377",
+        "7 bis": "C01387",
+        "8": "C01378",
+        "9": "C01379",
+        "10": "C01380",
+        "11": "C01381",
+        "12": "C01382",
+        "13": "C01383",
+        "14": "C01384",
+        "R": "C01731",
+        "H": "C01737",
+        "A": "C01742",
+        "B": "C01743",
+        "C": "C01727",
+        "D": "C01728",
+        "E": "C01729",
+        "P": "C01730",
+        "J": "C01739",
+        "N": "C01736",
+        "L": "C01740",
+        "K": "C01738",
+        "V": "C02711",
+        "U": "C01741",
+    }
+
+    return mappingLineToStif.get(line, "")
 
 
 
 
-def getLineInfo(apiKey:str,humainLine:str) -> list[list[dict]] | str:
+def getLineInfo(apiKey:str,humainLine:LineIDF) -> list[list[dict[str,object]]] | str:
     logger.info(f"Using API Key: {apiKey}")
 
-    selected_line = getLineStif(humainLine)
+    selected_line = getLineStif(humainLine.value)
 
     if(len(selected_line) <= 0) :
-      return "Désolé, nous ne connaissons pas la ligne" + humainLine
+      return "Désolé, nous ne connaissons pas la ligne" + humainLine.name
 
     header = {
         "Accept": "application/json",
@@ -100,8 +119,14 @@ def getLineInfo(apiKey:str,humainLine:str) -> list[list[dict]] | str:
         return f"Error fetching data for line {humainLine}: {result.status_code} - {result.text}"
     else:
         try:
+            jsonResult: dict[str, Any] = result.json()    
 
-            listGeneralMsg = result.json().get("Siri", {}).get("ServiceDelivery", {}).get("GeneralMessageDelivery", {})
+            siriData:dict[str,Any] = jsonResult.get("Siri",{})
+
+            if not isinstance(jsonResult.get("Siri"), dict):
+                return f"Invalid JSON response for line {humainLine}"
+
+            listGeneralMsg: list[dict[str,Any]] = dict(siriData.get("ServiceDelivery", {})).get("GeneralMessageDelivery", {})
 
             listInfos = [formatMsgIdf(msg.get("InfoMessage", [])) for msg in listGeneralMsg]
 
